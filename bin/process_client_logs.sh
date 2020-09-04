bc299cce5dbc6a2c1d932f6bd3bfa8dfd5d2ef8e
Halley Paulson <paul1098@morris.umn.edu>
2020-09-03 21:01:18 -0500

    Works but bats says no (\-3-)/

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -2,5 +2,12 @@
 #Takes a directory and outputs a text file with failed login data
 
 cd "$1"
+touch failed_login_data.txt
+ls|zcat *.tgz| awk '{
+	if(($6 == "Failed") && ($7 == "password") && !($9 == "invalid")) 
+		print $1,$2,$3,$9,$11;
 
-cat ls | awk 'match /invalid/ {print $1,$2,$3,$10,$12}' 
+	else
+		if(($6 == "Failed") && ($7 == "password") && ($9 == "invalid"))
+			print $1,$2,$3,$11,$13;
+		}'|sed 's/\:[0-9][0-9]//g' > failed_login_data.txt  
