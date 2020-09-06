59a4744dc13075d6169f08449bb5280e9f99d940
Halley Paulson <paul1098@morris.umn.edu>
2020-09-06 11:54:41 -0500

    create_username_dist works but bats dont pass still

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -2,12 +2,10 @@
 #Takes name of directory and creates file with tallied user failed log-ons
 
 cd "$1"
-cat *.txt | awk '{print $4}' | sort | uniq -c | awk '{printf "data.addRow([\x27"$1"\x27,"$2"])" }' > scratch.txt
+cat *.txt | awk '{print $4}' | sort | uniq -c | awk '{printf "data.addRow([\x27"$2"\x27,"$1"]);"\n }' > scratch.txt
 cd ..
 cd html_components
 ../wrap_contents.sh ../"$1"/scratch.txt username_dist username_dist.html
-mv username_dist.html "$1"
-cd ..
-cd "$1"
-rm scratch.txt
+mv username_dist.html ../"$1"
+rm ../"$1"/scratch.txt
 
