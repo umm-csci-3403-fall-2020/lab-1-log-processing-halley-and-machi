47e3ae2aadd20bfa30d36ca59fd05b636239854f
machiiwata <ksml1064>
2020-09-11 14:34:20 -0500

    Fix code accroding to shellcheck

--- bin/create_hours_dist.sh
+++ bin/create_hours_dist.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-cd "$1"
-cat **/*.txt | awk '{print $3}' | sort | uniq -c | awk '{printf "data.addRow([\x27"$2"\x27,"$1"]);\n" }' > scratch.txt
+cd "$1" || exit
+cat -- **/*.txt | awk '{print $3}' | sort | uniq -c | awk '{printf "data.addRow([\x27"$2"\x27,"$1"]);\n" }' > scratch.txt
+
 cd ..
 cd html_components
+
 ../bin/wrap_contents.sh ../"$1"/scratch.txt hours_dist ../"$1"/hours_dist.html
 cd ..
 rm "$1"/scratch.txt
